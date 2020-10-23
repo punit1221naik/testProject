7731d74420a8bc7f93bb17e0741c42ad672fb73d
Punit Naik <punit.robosoft@phonepe.com>
2020-10-23 14:11:25 +0530

    Initial Commit

---
+++ CheckTheProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckTheProject
+//
+//  Created by Punit Ramchandra Naik on 23/10/20.
+//  Copyright © 2020 Punit Ramchandra Naik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
